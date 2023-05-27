4818e3825959615d35fd2c2830185e135d526d49
HajkD <hajkdrost@googlemail.com>
2023-05-27 17:19:51 +0200

    update diamond installation check

--- R/is_diamond_installed.R
+++ R/is_diamond_installed.R
@@ -4,16 +4,16 @@ is_diamond_installed <- function(diamond_exec_path = NULL) {
         if (!is.null(diamond_exec_path))
             # e.g. diamond_exec_path = '/opt/miniconda3/bin/'
             sys_out <-
-            system(paste0(file.path(diamond_exec_path,"diamond")," version"), intern = TRUE)
+            system(paste0(file.path(diamond_exec_path,"diamond version")), intern = TRUE)
 
         if (is.null(diamond_exec_path))
             # e.g. diamond_exec_path = '/opt/miniconda3/bin/'
             sys_out <-
-                system("diamond"," version", intern = TRUE)
+                system("diamond version", intern = TRUE)
 
     }, error = function(e)
         stop(
-            "It seems like you don't have DIAMOND installed locally on your machine or the PATH variable to the DIAMOND program is not set correctly.",
+            "It seems like you don't have DIAMOND2 installed locally on your machine or the PATH variable to the DIAMOND2 program is not set correctly.",
             " Please use the 'diamond_exec_path' argument to specify a path to your diamond excecutable, e.g. diamond_exec_path = '/usr/local/bin' or diamond_exec_path = '/opt/miniconda3/bin'.",
             call. = FALSE
         ))
